d985785178a104d93a340c48624052ad8fa52954
vpoat <vpoat@pop-os.localdomain>
2021-04-30 08:01:46 +0700

    up

--- configs/ReDet/ReDet_re50_refpn_1x_shipdata_ms.py
+++ configs/ReDet/ReDet_re50_refpn_1x_shipdata_ms.py
@@ -203,7 +203,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 12
+total_epochs = 40
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = 'work_dirs/ReDet_re50_refpn_1x_dota1_ms'
